37bde164d5b4252916c23e935fc8e31e6968e24d
Ashley Waldner <ashleywaldner22@gmail.com>
2020-04-04 15:50:46 -0500

    add vars and add values to said vars add them to the firebase object

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,23 +6,75 @@ var config ={
     storageBucket: "train-schedule-72429.appspot.com"
 }
 
-firebase.intializeApp(config);
-
+firebase.initializeApp(config);
+// Create a variable to reference the database.
 var database = firebase.database();
-
-// Add connection for multiple browsers
+// connectionsRef references a specific location in our database.
+// All of our connections will be stored in this directory.
 var connectionsRef = database.ref("/connections");
-
+// '.info/connected' is a special location provided by Firebase that is updated
+// every time the client's connection state changes.
+// '.info/connected' is a boolean value, true if the client is connected and false if they are not.
 var connectedRef = database.ref(".info/connected");
-
+// When the client's connection state changes...
 connectedRef.on("value", function(snap) {
-    if (snap.val()) {
-
-        var con = connectionsRef.push(true);
-        con.onDisconnect().remove();
-    }
+  // If they are connected..
+  if (snap.val()) {
+    // Add user to the connections list.
+    var con = connectionsRef.push(true);
+    // Remove user from the connection list when they disconnect.
+    con.onDisconnect().remove();
+  }
+});
+// When first loaded or when the connections list changes...
+connectionsRef.on("value", function(snap) {
+  // Display the viewer count in the html.
+  // The number of online users is the number of children in the connections list.
+  $("#connected-viewers").text(snap.numChildren());
 });
 
-connectionsRef.on("value", function(snap){
-    $("#connected-viewers").text(snap(numChildren()));
+
+
+// Vars for inputs 
+var trainName = "";
+var destination = "";
+var trainTime = "";
+var frequency = "";
+
+
+// when submit is clicked, gets the info from the form
+$("#click-button").on("click", function(event){
+    //prevent the page from refreshing
+    event.preventDefault();
+
+    trainName = $("#trainName").val().trim();
+    destination = $("#destination").val().trim();
+    trainTime = $("#trainTime").val().trim();
+    frequency = $("#frequency").val().trim();
+
+    database.ref().set({
+        trainName: trainName,
+        destination: destination,
+        trainTime: trainTime,
+        frequency: frequency
+    });
 });
+
+database.ref().on("value", function(snapshot){
+    //print the initial data to the console
+    console.log(snapshot.val());
+
+    //log the value of the various properties
+    console.log(snapshot.val().trainName);
+    console.log(snapshot.val().destination);
+    console.log(snapshot.val().trainTime);
+    console.log(snapshot.val().frequency);
+
+    //change the HTML
+
+
+
+    
+}, function(errorObject){
+    console.log("The read failed: " + errorObject.code);
+})
