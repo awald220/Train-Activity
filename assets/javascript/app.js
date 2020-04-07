e781b2a5c50bd43e275b8e956fcc3f265398e9b7
Ashley Waldner <ashleywaldner22@gmail.com>
2020-04-07 11:16:15 -0500

    add new data to fire base stores multiple, got .push to work, content correctly prints to html

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,4 @@
-// $(document).ready(function(){
+$(document).ready(function(){
 var config ={
     apiKey: "AIzaSyCh0oepPxWJWV6wC1ih-bkqJN7w9lAmKc0",
     authDomain: "train-schedule-72429.firebaseapp.com",
@@ -8,70 +8,65 @@ var config ={
 }
 
 firebase.initializeApp(config);
-// Create a variable to reference the database.
-var database = firebase.database();
-// connectionsRef references a specific location in our database.
-// All of our connections will be stored in this directory.
-var connectionsRef = database.ref("/connections");
-// '.info/connected' is a special location provided by Firebase that is updated
-// every time the client's connection state changes.
-// '.info/connected' is a boolean value, true if the client is connected and false if they are not.
-var connectedRef = database.ref(".info/connected");
-// When the client's connection state changes...
-connectedRef.on("value", function(snap) {
-  // If they are connected..
-  if (snap.val()) {
-    // Add user to the connections list.
-    var con = connectionsRef.push(true);
-    // Remove user from the connection list when they disconnect.
-    con.onDisconnect().remove();
-  }
-});
-//When first loaded or when the connections list changes...
-connectionsRef.on("value", function(snap) {
-});
 
-// Initials Values
-var trainName;
-var destination;
-var trainTime;
-var frequency;
+// Assign the reference to the database to a variable named 'database'
+var database = firebase.database();
+    
+var name = "";
+var dest = "";
+var time = "";
+var freq = "";
+
+
+$("#click-button").on("click", function(event){
+    event.preventDefault();
+
+    name = $("#trainName").val().trim();
+    dest= $("#destination").val().trim();
+    time = $("#firstTrain").val().trim();
+    freq = $("#interval").val().trim();
+    
+    database.ref().push({
+        name: name,
+        dest: dest, 
+        time: time, 
+        freq: freq,
+        dateAdded: firebase.database.ServerValue.TIMESTAMP
+    });
+   console.log("hello")
+   $("form")[0].reset();
+})
 
 // At the page load and subsequent value changes, get a snapshot of the local data.
 // This function allows you to update your page in real-time when the values within the firebase node bidderData changes
-database.ref().on("value", function(snapshot){
-  console.log(snapshot.val());
-  
-  trainName = snapshot.val().trainName;
-  destination = snapshot.val().destination;
-  trainTime = snapshot.val().trainTime;
-  frequency = snapshot.val().frequency;
+database.ref().on("child_added", function(snapshot){
+ //print the local data to the console
+  console.log(snapshot.val())
 
+  var sv = snapshot.val()
+  
+  //give the following vars value of what they would be in the database object
+  var trainName = sv.trainName;
+  var destination = sv.destination;
+  var trainTime = sv.trainTime;
+  var frequency = sv.frequency;
+
+  // add new row when info is inputed to form display on the page 
   var newRow = $("<tr>");
  $("tbody").append(newRow);
- var newTrainName = $("<td>").text(trainName);
- var newDestintion = $("<td>").text(destination);
- var newTrainTime = $("<td>").text(trainTime);
- var newFrequency = $("<td>").text(frequency);
+ var newTrainName = $("<td>").text(sv.name);
+ var newDestintion = $("<td>").text(sv.dest);
+ var newTrainTime = $("<td>").text(sv.time);
+ var newFrequency = $("<td>").text(sv.freq);
  $(newRow).append(newTrainName, newDestintion, newFrequency, newTrainTime);
 
 })
 
-$("#click-button").on("click", function(event){
-  event.preventDefault();
-
-  database.ref("/trainData").set({
-    trainName: trainName,
-    destination: destination,
-    trainTime: trainTime,
-    frequency: frequency
-  });
-
-})
 
 
 
 
 
 
-// }); //document ready 
+
+}); //document ready 
